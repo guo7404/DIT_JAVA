14778746f1e6962eadd6dfa9be6ebd886378098e
김현준 <102206431+guo7404@users.noreply.github.com>
2024-09-29 22:53:44 +0900

    Add files via upload

--- report2/src/report2_3/report2_4.java
+++ report2/src/report2_3/report2_4.java
@@ -3,7 +3,14 @@ package report2_3;
 public class report2_4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int n [][] = {{1,2,3},{1,2},{1},{1,2,3},{1,2,3,4}};
+//		System.out.print(n.length);
+		for(int i=0;i<n.length;i++) {
+			for(int j=0;j<n[i].length;j++) {
+				System.out.print(n[i][j]+" ");
+			}
+			System.out.println("");
+		}
 
 	}
 
